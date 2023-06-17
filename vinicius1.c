b125330d24d2973277d4b06f27c45459979efb6b
viniciumoren <128702958+viniciumoren@users.noreply.github.com>
2023-06-17 16:06:43 -0300

    Update and rename vinicius.c to vinicius1.c

--- vinicius1.c
+++ vinicius1.c
@@ -8,7 +8,7 @@ int main()
 
     int i, j;
     float nota[3][3];
-    float media[2];
+    float media[3];
     float soma;
     char disciplina[3][250];
     float peso[3] = {3, 3, 4};
@@ -20,28 +20,41 @@ int main()
 
         system("cls");
 
+        fflush(stdin);
+
         for (j = 0; j < 3; j++)
         {
+            
+            do
+            {
             printf("Digite sua nota %dª :", j + 1);
-            scanf("%f", &nota[i][j]);
+            scanf("%f", &nota[i][j]);            
+
+            system("cls");
+          
+            } while (nota[i][j]>10 );
 
             soma += (nota[i][j]);
 
-            system("cls");
+            fflush(stdin);
+            
+            
         }
 
-        media[i] = soma / 10;
+        media[i] = soma / j;
         soma = 0;
+
+        fflush(stdin);
     }
 
     for (i = 0; i < 3; i++)
     {
-        printf("\n Nome da disciplina: %s \n", disciplina[i]);
+        printf("\n Disciplina: %s \n", disciplina[i]);
 
         for (j = 0; j < 3; j++)
         {
-            printf("Sua nota %dª : %.f\n", j + 1, nota[i][j]);
+            printf("Sua nota %dÂª : %.f\n", j + 1, nota[i][j]);
         }
-        printf("\nMédia: %.1\n", media[i]);
+        printf("\nMedia: %.1f\n", media[i]);
     }
 }
